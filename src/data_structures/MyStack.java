ca82e59e1ea470944e3a725ce696f794720512ed
Mohammad <m_vahidalizadeh@live.com>
2016-06-18 21:47:47 -0400

    Stack example has been added

---
+++ src/data_structures/MyStack.java
@@ -0,0 +1,34 @@
+package data_structures;
+
+import java.util.Stack;
+
+/**
+ * Created by Mohammad on 6/18/2016.
+ */
+public class MyStack {
+        public static void main(String args[]) {
+            // creating stack
+            Stack st = new Stack();
+
+            // populating stack
+            st.push("Mohammad");
+            st.push("Ali");
+            st.push("Mahnaz");
+            st.push("Parinaz");
+            st.push("Vahid");
+
+            // removing top object
+            System.out.println("Removed object is: "+st.pop());
+
+            // elements after remove
+            System.out.println("Elements after remove: "+st);
+
+            // Peak
+            System.out.println("Peak: "+st.peek());
+
+            // elements after push
+            st.push("Zahra");
+            System.out.println("Elements after pushing Zahra: "+st);
+
+        }
+    }
