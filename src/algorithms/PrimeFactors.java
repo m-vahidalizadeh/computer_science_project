c068c42c180b523bd440bc2fc61f239abde4077d
Mohammad <m_vahidalizadeh@live.com>
2016-06-19 03:11:56 -0400

    Some algorithms have been added

---
+++ src/algorithms/PrimeFactors.java
@@ -0,0 +1,40 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Mohammad on 6/19/2016.
+ */
+public class PrimeFactors {
+    public static List<Integer> primeFactors(int numbers) {
+        int n = numbers;
+        List<Integer> factors = new ArrayList<Integer>();
+        for (int i = 2; i <= n / i; i++) {
+            while (n % i == 0) {
+                factors.add(i);
+                n /= i;
+            }
+        }
+        if (n > 1) {
+            factors.add(n);
+        }
+        return factors;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Primefactors of 44");
+        for (Integer integer : primeFactors(44)) {
+            System.out.println(integer);
+        }
+        System.out.println("Primefactors of 3");
+        for (Integer integer : primeFactors(3)) {
+            System.out.println(integer);
+        }
+        System.out.println("Primefactors of 32");
+        for (Integer integer : primeFactors(32)) {
+            System.out.println(integer);
+        }
+    }
+}
+
