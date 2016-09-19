7cf85e76bf1ce1f50b46632915aa845d60155254
Mohammad <m_vahidalizadeh@live.com>
2016-09-19 12:08:21 -0400

    Min heap and max heap with priority queue have been added

---
+++ src/data_structures/MinHeapWithPriorityQueue.java
@@ -0,0 +1,27 @@
+package data_structures;
+
+import java.util.PriorityQueue;
+
+/**
+ * Created by Mohammad on 9/19/2016.
+ */
+public class MinHeapWithPriorityQueue {
+
+    public static void main(String args[]) {
+        // create priority queue
+        PriorityQueue< Integer > prq = new PriorityQueue <> ();
+
+        // insert values in the queue
+        prq.add(6);
+        prq.add(9);
+        prq.add(5);
+        prq.add(64);
+        prq.add(6);
+
+        //print values
+        while (!prq.isEmpty()) {
+            System.out.print(prq.poll()+" ");
+        }
+    }
+
+}
